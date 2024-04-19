a61b371d91826e626568cd5df43276f7c0b19814
PATRICK HESSION <G00394619@gmit.ie>
2024-04-19 05:43:29 +0100

    added profile pic to profile screen

--- App/Screen/Profile/ProfileScreen.jsx
+++ App/Screen/Profile/ProfileScreen.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { useUser } from '@clerk/clerk-react';
 
 export default function ProfileScreen() {
+  const { user } = useUser();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 50 }}>
+      {user?.imageUrl && (
+        <Image
+          source={{ uri: user.imageUrl }}
+          style={{ width: 100, height: 100, borderRadius: 50 }}
+        />
+      )}
+      <Text style={{ marginTop: 15, fontWeight: 'bold', fontSize: 20 }}>{user?.fullName}</Text>
     </View>
-  )
+  );
 }
