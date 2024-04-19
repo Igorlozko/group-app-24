010682466319a098c9d162bd1435c9d54384d0ed
PATRICK HESSION <G00394619@gmit.ie>
2024-04-19 07:06:53 +0100

    Refactor Header component and add SafeAreaView in Header.jsx

--- App/Screen/Home/Header.jsx
+++ App/Screen/Home/Header.jsx
@@ -2,21 +2,22 @@ import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-react'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native';
 
 export default function Header() {
-    const {user} = useUser();
+  const {user} = useUser();
   return (
-    <View
-       style={styles.container}
-    >
-      <Image source={{uri:user?.imageUrl}}
-        style={{width:45,height:45,borderRadius:99}}
-      />
-      <Image source ={require('./../../../assets/images/logo111.png')}
-        style={{width:45,height:45,objectFit:'contain'}}
-      />
-      <MaterialCommunityIcons name="filter" size={30} color="black" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image source={{uri:user?.imageUrl}}
+          style={{width:45, height:45, borderRadius:99, marginRight: 10}} // Add marginRight
+        />
+        <Image source ={require('./../../../assets/images/logo111.png')}
+          style={{width:45,height:45,objectFit:'contain'}}
+        />
+        <MaterialCommunityIcons name="filter" size={30} color="black" />
+      </View>
+    </SafeAreaView>
   )
 }
 
