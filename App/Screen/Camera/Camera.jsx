44878ff018dd1d4817f2fec593426f735f4a5440
PATRICK HESSION <G00394619@gmit.ie>
2024-04-23 01:12:40 +0100

    Add flash and cameraRef to CameraScreen in Camera.jsx

--- App/Screen/Camera/Camera.jsx
+++ App/Screen/Camera/Camera.jsx
@@ -1,10 +1,12 @@
-import{Camera, CameraType} from 'expo-camera'
+import{Camera, CameraType, useRef} from 'expo-camera'
 import * as MediaLibrary from 'expo-media-library';
 
 export default function CameraScreen(){
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
 const [image, setImage] = useState(null);
 const [type , setType] = useState(Camera.Constants.Type.back);
+const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+const cameraRef = useRef(null);
   return(
     <View style={styles.container}>
     </View>
