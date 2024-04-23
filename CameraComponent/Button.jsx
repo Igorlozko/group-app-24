fa0ec19809bbb649bb119ac8453798c4cb000b7d
PATRICK HESSION <G00394619@gmit.ie>
2024-04-23 01:42:02 +0100

    Refactor Button component styles in Button.jsx

--- CameraComponent/Button.jsx
+++ CameraComponent/Button.jsx
@@ -10,3 +10,21 @@ export default function Button{{title, onPress, icon, colour}} {
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        flexDirection: 'row',
+        backgroundColor: '#f1f1f1',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 20,
+        marginLeft: 10,
+    colour: '#f1f1f1',
+    marginLeft: 10
+
+}
+ })
