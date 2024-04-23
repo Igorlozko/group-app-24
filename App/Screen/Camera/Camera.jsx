11c4eee3a389aac37295bbc7542e4abc1b21effe
PATRICK HESSION <G00394619@gmit.ie>
2024-04-23 01:33:29 +0100

    Refactor Camera component in Camera.jsx

--- App/Screen/Camera/Camera.jsx
+++ App/Screen/Camera/Camera.jsx
@@ -1,42 +1,53 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import{Camera, CameraType} from 'expo-camera'
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 
-export default function CameraScreen(){
-const [hasCameraPermission, setHasCameraPermission] = useState(null);
-const [image, setImage] = useState(null);
-const [type , setType] = useState(Camera.Constants.Type.back);
-const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
-const cameraRef = useRef(null);
+export default function CameraScreen() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
 
-useEffect(()=>{
-(async ()=>{
-  MediaLibrary.requestPermissionsAsync();
-  const cameraStatus = await Camera.requestPermissionsAsync();
-  setHasCameraPermission(cameraStatus.status === 'granted');
-})();
-}, []);
+  useEffect(() => {
+    (async () => {
+      await MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, []);
 
-  return(
+  return (
     <View style={styles.container}>
-      <Camera
-      styles={styles.camera}
-      type={type}
-      FlashMode={flash}
-      ref={cameraRef}
-      >
-       <Text> hello</Text>
-        </Camera>
+      <Camera style={styles.camera} type={type} flashMode={flash} ref={cameraRef}>
+        <Text> hello</Text>
+      </Camera>
+      <View>
+        <Button
+          title="Take Picture"
+          onPress={async () => {
+            if (cameraRef.current) {
+              const photo = await cameraRef.current.takePictureAsync();
+              console.log(photo);
+              setImage(photo.uri);
+              MediaLibrary.saveToLibraryAsync(photo.uri);
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#fff',
-    alignItems:'center',
-    justifyContent:'center'
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+    borderRadius: 20,
+  },
 });
