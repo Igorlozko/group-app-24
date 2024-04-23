b75122a78cc319bb9a721070ca0e151f27c86e35
PATRICK HESSION <G00394619@gmit.ie>
2024-04-23 01:16:25 +0100

    Refactor CameraScreen component in Camera.jsx

--- App/Screen/Camera/Camera.jsx
+++ App/Screen/Camera/Camera.jsx
@@ -1,12 +1,23 @@
-import{Camera, CameraType, useRef} from 'expo-camera'
+import React, {useState, useRef, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import{Camera, CameraType} from 'expo-camera'
 import * as MediaLibrary from 'expo-media-library';
 
 export default function CameraScreen(){
-  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+const [hasCameraPermission, setHasCameraPermission] = useState(null);
 const [image, setImage] = useState(null);
 const [type , setType] = useState(Camera.Constants.Type.back);
 const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
 const cameraRef = useRef(null);
+
+useEffect(()=>{
+(async ()=>{
+  MediaLibrary.requestPermissionsAsync();
+  const cameraStatus = await Camera.requestPermissionsAsync();
+  setHasCameraPermission(cameraStatus.status === 'granted');
+})();
+}, []);
+
   return(
     <View style={styles.container}>
     </View>
